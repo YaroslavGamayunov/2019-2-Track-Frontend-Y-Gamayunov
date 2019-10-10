10c48e78902977ea5b17d413ef68a137642e447d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-10 15:47:45 +0000

    Deploy YaroslavGamayunov/2019-2-Track-Frontend-Y-Gamayunov to github.com/YaroslavGamayunov/2019-2-Track-Frontend-Y-Gamayunov.git:gh-pages

---
+++ precache-manifest.796a981bd16ddb26db48c13962d7aa6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8228defeb6b18377da2c00d50f422916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf43823cf0bfa2c688bc",
+    "url": "/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "7e243ab618838845009f",
+    "url": "/static/js/2.0fcb7f57.chunk.js"
+  },
+  {
+    "revision": "bf43823cf0bfa2c688bc",
+    "url": "/static/js/main.0385953e.chunk.js"
+  },
+  {
+    "revision": "d9a1a84e70ca97b86732",
+    "url": "/static/js/runtime-main.7bdc4dff.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/static/media/logo.795fb555.svg"
+  }
+]);
